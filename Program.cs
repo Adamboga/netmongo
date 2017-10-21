d75ec13ecec0bc00b9d524e565010f8d48b0b62b
Adam <adam10ar@yahoo.com>
2017-10-21 03:30:37 -0400

    new project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace netmongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("ppl");
+            var collection = database.GetCollection<BsonDocument>("info");
+
+            var documnt = new BsonDocument{
+                { "name": "Adam"},
+                { "age": "38"},
+                { "sex": "Male"}
+            };
+         }
+    }
+}
